6db98964952bb3b3e5ab2716b5f5252bb952edcb
Babajide Ogunjobi <jogunjobi@gmail.com>
2023-11-01 16:52:40 -0500

    update

--- start.sh
+++ start.sh
@@ -3,26 +3,25 @@
 # Build image
 docker build -t app .
 
+minikube start --forc
+
+# Enable ingress
+minikube addons enable ingress
+
 # Load image into minikube
 minikube image load app
 
 # Apply Kubernetes YAML
 kubectl apply -f k8s.yaml
 
-# Port forward Prometheus
-kubectl port-forward svc/prometheus 9090:9090 &
+# Deploy to Kubernetes
+kubectl apply -f deployment.yaml
+kubectl apply -f service.yaml
+kubectl apply -f ingress.yaml
 
-# Configure Prometheus to scrape app metrics
-cat <<EOF | kubectl apply -f -
-apiVersion: monitoring.coreos.com/v1
-kind: ServiceMonitor
-metadata:
-  name: app-monitor
-spec:
-  selector:
-    matchLabels:
-      app: counter
-  endpoints:
-  - port: metrics
-    interval: 15s
-EOF
+# Port forward Prometheus
+#kubectl port-forward svc/prometheus 9090:9090 &
+
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm install prometheus prometheus-community/prometheus
+kubectl annotate svc flask-service prometheus.io/scrape="true" prometheus.io/port="8080"
